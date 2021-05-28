12e25d5439332b8074430063a80c11662eb61802
lhs <leonardo.schonardt@matera.com>
2021-05-28 15:09:18 -0300

    Roteamento de diversos microsservicos com zuul

--- hr-payroll/src/main/java/com/hrworker/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/hrworker/hrpayroll/resources/PaymentResource.java
@@ -1,6 +1,8 @@
 package com.hrworker.hrpayroll.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hrworker.hrpayroll.entities.Payment;
 import com.hrworker.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -19,9 +22,13 @@ public class PaymentResource {
 		this.paymentService = paymentService;
 	}
 	
+	@HystrixCommand(fallbackMethod = "getPaymentFallback")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable(value = "workerId") long workerId, @PathVariable(value = "days") int days) {
 		return ResponseEntity.ok(paymentService.getPayment(workerId, days));
 	}
 	
+	public ResponseEntity<Payment> getPaymentFallback(long workerId, int days) {
+		return ResponseEntity.ok(new Payment("Ana", 296.0, days));
+	}
 }
